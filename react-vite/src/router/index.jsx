2fe5c3c504bf055c0982537669da41b85a4daefd
kyle <averytkyle@gmail.com>
2025-02-26 19:43:21 -0500

    hid the api key

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -5,8 +5,6 @@ import Businesses from '../components/Businessess/Businesses';
 import MapContainer from '../components/Maps';
 import Layout from './Layout';
 
-const API_KEY = 'AIzaSyAw7P1sr8XkoP4zrOwof9Hzl1uEDgXmLhk';
-
 export const router = createBrowserRouter([
   {
     element: <Layout />,
